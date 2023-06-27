365def7cc968b1d2299acd62d7859bf810c2e5b5
Arpit Mishra <Arpit.Mishra@Geminisolutions.com>
2023-06-27 16:48:40 +0530

    changes

--- src/main/java/StepDefination/Hook.java
+++ src/main/java/StepDefination/Hook.java
@@ -21,9 +21,15 @@ public class Hook {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox");
         DriverManager.initializeChrome(options);
         DriverAction.maximizeToDefaultBrowserSize();
-        DriverAction.launchUrl(GemJarUtils.getGemJarConfigData("launchUrl"));
+        DriverAction.launchUrl(GemJarUtils.getGemJarConfigData("launch_url"));
         DriverAction.setImplicitTimeOut(Long.parseLong(GemJarGlobalVar.implicitTime));
         DriverAction.setPageLoadTimeOut(Long.parseLong(GemJarGlobalVar.pageTimeout));
         DriverAction.setScriptTimeOut(Long.parseLong(GemJarGlobalVar.scriptTimeout));
