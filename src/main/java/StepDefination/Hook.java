bf7a9fbd582509105805d813c47f01457a7367dc
Arpit Mishra <102647340+gem-arpitmishra@users.noreply.github.com>
2023-08-31 17:36:32 +0530

    Update Hook.java

--- src/main/java/StepDefination/Hook.java
+++ src/main/java/StepDefination/Hook.java
@@ -17,22 +17,22 @@ import java.util.logging.Logger;
 public class Hook {
     @Before
     public static void hook() throws GemException {
-        DriverManager.setUpBrowser();
-//        WebDriverManager.chromedriver().setup();
-//        ChromeOptions options = new ChromeOptions();
-//        options.setHeadless(true);
-//        options.addArguments("start-maximized"); // open Browser in maximized mode
-//        options.addArguments("disable-infobars"); // disabling infobars
-//        options.addArguments("--disable-extensions"); // disabling extensions
-//        options.addArguments("--disable-gpu"); // applicable to windows os only
-//        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-//        options.addArguments("--no-sandbox");
-//        DriverManager.initializeChrome(options);
-//        DriverAction.maximizeToDefaultBrowserSize();
-//        DriverAction.launchUrl(GemJarUtils.getGemJarConfigData("launch_url"));
-//        DriverAction.setImplicitTimeOut(Long.parseLong(GemJarGlobalVar.implicitTime));
-//        DriverAction.setPageLoadTimeOut(Long.parseLong(GemJarGlobalVar.pageTimeout));
-//        DriverAction.setScriptTimeOut(Long.parseLong(GemJarGlobalVar.scriptTimeout));
+        // DriverManager.setUpBrowser();
+       WebDriverManager.chromedriver().setup();
+       ChromeOptions options = new ChromeOptions();
+       options.setHeadless(true);
+       options.addArguments("start-maximized"); // open Browser in maximized mode
+       options.addArguments("disable-infobars"); // disabling infobars
+       options.addArguments("--disable-extensions"); // disabling extensions
+       options.addArguments("--disable-gpu"); // applicable to windows os only
+       options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+       options.addArguments("--no-sandbox");
+       DriverManager.initializeChrome(options);
+       DriverAction.maximizeToDefaultBrowserSize();
+       DriverAction.launchUrl(GemJarUtils.getGemJarConfigData("launch_url"));
+       DriverAction.setImplicitTimeOut(Long.parseLong(GemJarGlobalVar.implicitTime));
+       DriverAction.setPageLoadTimeOut(Long.parseLong(GemJarGlobalVar.pageTimeout));
+       DriverAction.setScriptTimeOut(Long.parseLong(GemJarGlobalVar.scriptTimeout));
     }
     private final static Logger logger = Logger.getLogger(String.valueOf(GemEcoUpload.class));
 }
